ef3100ea33c33991defdefac43cd0e7569a501cc
Andreas Struller <andreas@potyomkin.org>
2014-05-29 16:36:07 +0200

    Updated.

--- convenience_plugin.py
+++ convenience_plugin.py
@@ -1,7 +1,8 @@
 from base_plugin import SimpleCommandPlugin
-from core_plugins.player_manager import permissions, UserLevels
-from packets import warp_command_write, Packets
-from utility_functions import build_packet
+from plugins.core.player_manager import permissions, UserLevels
+from packets import warp_command_write, Packets, warp_command
+from utility_functions import build_packet, extract_name
+
 
 class Convenience(SimpleCommandPlugin):
     """
@@ -15,25 +16,22 @@ class Convenience(SimpleCommandPlugin):
     def activate(self):
         super(Convenience, self).activate()
         self.player_manager = self.plugins['player_manager'].player_manager
-    
-    @permissions(UserLevels.GUEST)
-    def ship(self, name):
-        """Warps you to your ship. Syntax: /ship"""
-        self.logger.debug("Ship command called by %s", self.protocol.player.name)
-        name = self.protocol.player.name
-        target_player = self.player_manager.get_logged_in_by_name(name)
-        target_protocol = self.protocol.factory.protocols[target_player.protocol]
-        warp_packet = build_packet(Packets.WARP_COMMAND,
-                                   warp_command_write(t='WARP_UP'))
-        self.protocol.client_protocol.transport.write(warp_packet)
 
-    @permissions(UserLevels.GUEST)
-    def home(self, name):
-        """Warps you to your home planet. Syntax: /home"""
-        self.logger.debug("Ship command called by %s", self.protocol.player.name)
-        name = self.protocol.player.name
-        target_player = self.player_manager.get_logged_in_by_name(name)
-        target_protocol = self.protocol.factory.protocols[target_player.protocol]
-        warp_packet = build_packet(Packets.WARP_COMMAND,
-                                   warp_command_write(t='WARP_HOME'))
-        self.protocol.client_protocol.transport.write(warp_packet)
+    @permissions(UserLevels.REGISTERED)
+    def ship(self, arg):
+        """Warps you to your ship.\nSyntax: /ship"""
+        player = self.player_manager.get_logged_in_by_name(self.protocol.player.name)
+        from_protocol = self.factory.protocols[player.protocol]
+        warp_packet = build_packet(Packets.WARP_COMMAND, warp_command_write(t='WARP_UP'))
+        from_protocol.client_protocol.transport.write(warp_packet)
+        self.protocol.send_chat_message("^yellow;%s^green; warped to their ship." % self.protocol.player.name)
+
+    @permissions(UserLevels.REGISTERED)
+    def home(self, arg):
+        """Warps you to your home planet.\nSyntax: /home"""
+        player = self.player_manager.get_logged_in_by_name(self.protocol.player.name)
+        from_protocol = self.factory.protocols[player.protocol]
+        warp_packet = build_packet(Packets.WARP_COMMAND, warp_command_write(t='WARP_HOME'))
+        from_protocol.client_protocol.transport.write(warp_packet)
+        self.protocol.send_chat_message("^yellow;%s^green; warped to their home planet." % self.protocol.player.name)
+
